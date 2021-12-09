0d7c074011e84955167cfe4470262bc09760d693
dprrrr <dprrrr1987@mail.ru>
2021-12-09 17:43:52 +0300

    проект

---
+++ Пример/vosmerochka_ry/vosmerochka_ry/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vosmerochka_ry
+{
+    public partial class Form1 : Form
+    {
+        connect con = new connect();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query1 = "SELECT *  FROM productmaterial_k_import ";
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = con.CO(query1).Tables[0];
+            string query2 = "SELECT *  FROM products_k_import";
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.DataSource = con.CO(query2).Tables[0];
+            string query3 = "SELECT *  FROM materials_short_k_import ";
+            dataGridView3.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView3.AllowUserToAddRows = false;
+            dataGridView3.DataSource = con.CO(query3).Tables[0];
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WpfElementi we = new WpfElementi();
+            we.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Пример/vosmerochka_ry/vosmerochka_ry/WpfElementi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vosmerochka_ry
+{
+    public partial class WpfElementi : Form
+    {
+        UserControl1 wpfControl = new UserControl1();
+        UserControl2 wpfControl1 = new UserControl2();
+
+        public WpfElementi()
+        {
+            InitializeComponent();
+            elementHost1.Child = wpfControl1;
+            elementHost2.Child = wpfControl;
+        }
+
+        private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void WpfElementi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
